b9cf0554bb59210761d66dfdb4773ea3c4c7fbff
talrejah02 <talrejah02gmail.com>
2021-05-15 20:27:45 +0530

    first

--- src/components/sidebar/Chats.js
+++ src/components/sidebar/Chats.js
@@ -4,43 +4,52 @@ import {firestore} from '../../firebase'
 import './style.css'
 import {Card} from './Card/Card'
 import {useGroups} from '../../context/groups'
+import {auth} from '../../firebase'
+import firebase from 'firebase/app'
 
 
 
 function Chats() {
    const [Chats , setChats]=useState([])
    const {groups}=useGroups()
+   const [newGroup,setNewgroup] = useState({
+   title:null,
+   adding:false,
+   admin:null,
+ })
+
+
    
   useEffect( async() => {
      const allChats = await firestore.collection('group').get();
      allChats.docs.map(item =>!Chats.find(element=>element.id === item.id) && setChats( (group)=>[...group,{...item.data(),id: item.id}] ) )
-    },[groups.id])
-     
-    
+    },[groups])
 
+    const createGroup = async() => {
+       setNewgroup({...newGroup,adding:true})
+       if(newGroup.title !== ""){
 
+            await firestore.collection('group').add({
+               title: newGroup.title,
+               admin:auth.currentUser.uid,
+               createAt:firebase.firestore.FieldValue.serverTimestamp(),
+            })
+            setNewgroup({...newGroup,adding:false,title:""})
+         }
 
-
-     
-    
+      }
 
     return (
         <div className="chat-list">
            <p className="Chats-head">Chats</p>
            {/* <Online/> */}
-           {/* <div>
-              <input type="text"/>
-           </div> */}
+           <div className="create">
+              { newGroup.adding && <input className="create-input"   type="text" onChange={(e)=>setNewgroup({...newGroup,title:e.target.value })}/>}
+              <button className="create-btn"  onClick={createGroup}>Create Group</button>
+           </div>
            <div className="cards-container">
-           <p className="recent"> Recent</p>
-
-          
-
-          
-              
+           <p className="recent"> Recent</p>    
               {Chats.map(element => <Card element={element} />)}
-          
-          
            </div>
             
         </div>
